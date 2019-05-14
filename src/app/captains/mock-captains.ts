1b5ed04502dce49968d517fd3a92999386efb70d
John Griffiths <john@uxgent.com>
2019-05-14 13:52:49 -0400

    rename a captain.

--- src/app/captains/mock-captains.ts
+++ src/app/captains/mock-captains.ts
@@ -3,6 +3,6 @@ import { Captain } from './captain'
 export const CAPTAINS: Captain[] = [
   { id: 11, name: 'Richard Basehart' },
   { id: 12, name: 'Rod Serling' },
-  { id: 13, name: 'William Shatner' },
+  { id: 13, name: 'Robert Redford' },
   { id: 14, name: 'Inger Stevens' }
 ];
